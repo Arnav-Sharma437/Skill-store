fa7cbeb45c442e6bb6880f7faf70d0a225736a0c
Arnav Sharma <aarnavsharma437@gmail.com>
2026-08-31 12:27:07 +0530

    fix: remove watermark quote icon overlapping behind verified badge on testimonial cards

--- src/components/home/TestimonialsSection.tsx
+++ src/components/home/TestimonialsSection.tsx
@@ -130,13 +130,6 @@ export default function TestimonialsSection() {
                   tabIndex={0}
                   aria-label={`Testimonial from ${item.name}`}
                 >
-                  {/* Quote Icon Background */}
-                  <div className={styles.quoteIcon}>
-                    <svg width="40" height="40" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
-                    </svg>
-                  </div>
-
                   {/* Star Ratings & Badge */}
                   <div className={styles.cardTop}>
                     <div className={styles.starsRow}>
